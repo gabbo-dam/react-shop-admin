260de8e19f4823db71610c3c873b692bb47e27e0
Gabriel <gabbomorales1@gmail.com>
2022-09-01 04:14:08 -0500

    checkpoint

---
+++ src/pages/_app.js
@@ -0,0 +1,17 @@
+import '@styles/tailwind.css';
+import MainLayout from '@layout/mainLayout';
+import { ProviderAuth } from '@hooks/useAuth';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <ProviderAuth>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ProviderAuth>
+    </>
+  );
+}
+
+export default MyApp;
